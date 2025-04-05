5b4292459268ba21f4060ad3adfa1e1819cdbc21
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2025-04-05 22:20:17 +0700

    fix(wip): update config files

--- kit_app/premake5.lua
+++ kit_app/premake5.lua
@@ -4,6 +4,8 @@ repo_build = require("omni/repo/build")
 -- Repo root
 root = repo_build.get_abs_path(".")
 
+workspace "DefaultWorkspace"
+
 -- Execute the kit template premake configuration, which creates the solution, finds extensions, etc.
 dofile("_repo/deps/repo_kit_tools/kit-template/premake5.lua")
 
